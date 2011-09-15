b8f937838aa14558d5ad18b6c4235278011c72df
Ryan Resella <ryan@codeforamerica.org>
2011-09-14 23:41:27 -0700

    Added RiskFactor association

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,6 +3,6 @@ class Student < ActiveRecord::Base
   belongs_to :drug
   belongs_to :race
   belongs_to :school
-  belongs_to :risk
+  belongs_to :risk_factor
   belongs_to :updated_school, :class_name => "School", :foreign_key => "school_id"
 end
