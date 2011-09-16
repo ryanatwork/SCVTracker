58ce1b3dcc2c67a47078ae6d2354f5336c8a7418
Ryan Resella <ryan@codeforamerica.org>
2011-09-15 23:47:01 -0700

    Updated email address in user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:email){|n| "email#{n}@factory.com" }
+    sequence(:email){|n| "email#{n}@test.com" }
     password "123456"
     password_confirmation "123456"
   end
