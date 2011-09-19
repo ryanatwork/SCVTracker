bfd7b1555971d959e64e027eda725973c76bd456
Ryan Resella <ryan@codeforamerica.org>
2011-09-18 22:11:55 -0700

    Added relationship from Users to students

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
+  has_many :students, :class_name => "Student", :foreign_key => "school_deputy_id"
+
   def name
     self.first_name + ' ' + self.last_name
   end
