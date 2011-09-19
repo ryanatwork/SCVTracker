d09e9853229868b0b42cf86b92eecea28ca97e9a
Ryan Resella <ryan@codeforamerica.org>
2011-09-18 22:05:16 -0700

    Added User relationship on Student

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,4 +5,5 @@ class Student < ActiveRecord::Base
   belongs_to :school
   belongs_to :risk_factor
   belongs_to :updated_school, :class_name => "School", :foreign_key => "updated_school_id"
+  belongs_to :deputy, :class_name => "User", :foreign_key => "school_deputy_id"
 end
