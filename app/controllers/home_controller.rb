c9982bba017327d1d7d7325e2352c6a3b1250817
Ryan Resella <ryan@codeforamerica.org>
2011-09-18 22:26:33 -0700

    Added current assigned students

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    if current_user.school_deputy
+      @students = current_user.students
+    end
   end
 
 end
