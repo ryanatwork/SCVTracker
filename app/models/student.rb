4d5a694fa4e4f2247671c0e1757e9cbcb69a59ec
Ryan Resella <ryan@codeforamerica.org>
2011-10-01 11:03:18 -0700

    Added default image for student

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,7 +7,9 @@ class Student < ActiveRecord::Base
   belongs_to :updated_school, :class_name => "School", :foreign_key => "updated_school_id"
   belongs_to :deputy, :class_name => "User", :foreign_key => "school_deputy_id"
 
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+                    :default_url => 'default_student.jpg'
+                    #:default_url => 'missing_:style.png'
 
   def age(dob, now = Time.now.utc.to_date)
     now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
