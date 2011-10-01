345db5bbd45bc25406a1c3a23b99fafd99875e4d
Ryan Resella <ryan@codeforamerica.org>
2011-10-01 10:33:08 -0700

    Wrote spec for student age

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Student do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "age" do
+    it "should return the users age" do
+      @age = Student.new
+      student_age = @age.age(Date.new(1986,9,7), Date.new(2011,1,1))
+      student_age.should == 24
+    end
+  end
 end
