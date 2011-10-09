bcd912048cfe94ccab471156a078610d52ec8430
Ryan Resella <ryan@codeforamerica.org>
2011-10-08 22:36:02 -0700

    Added Date received search

--- app/assets/javascripts/crime_tips.js
+++ app/assets/javascripts/crime_tips.js
@@ -5,4 +5,5 @@ $(document).ready(function() {
   $( "#crime_tip_date_due" ).datepicker({ dateFormat: 'yy-mm-dd' });
   $( "#crime_tip_date_assigned" ).datepicker({ dateFormat: 'yy-mm-dd' });
   $( "#crime_tip_date_completed" ).datepicker({ dateFormat: 'yy-mm-dd' });
+  $("#search_date_rec_gte").datepicker({ dateFormat: 'yy-mm-dd' });
 });
