04ca06972b91494f61a4694f0b035e2e3a3fe336
Ryan Resella <ryan@codeforamerica.org>
2011-10-09 16:49:44 -0700

    Wrote method for suspect_name

--- app/models/crime_tip.rb
+++ app/models/crime_tip.rb
@@ -4,4 +4,9 @@ class CrimeTip < ActiveRecord::Base
   belongs_to :drug
   belongs_to :suspect_race, :class_name => "Race", :foreign_key => "suspect_race_id"
   belongs_to :suspect_school, :class_name => "School", :foreign_key => "suspect_school_id"
+
+  def suspect_name
+    suspect_name = self.suspect_first_name + ' ' + self.suspect_last_name
+  end
+
 end
