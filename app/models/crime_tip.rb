df2db63efbeea3ecdc2376653be86764c90a2286
Ryan Resella <ryan@codeforamerica.org>
2011-10-09 18:01:51 -0700

    Added geocoding for crime tips

--- app/models/crime_tip.rb
+++ app/models/crime_tip.rb
@@ -5,8 +5,16 @@ class CrimeTip < ActiveRecord::Base
   belongs_to :suspect_race, :class_name => "Race", :foreign_key => "suspect_race_id"
   belongs_to :suspect_school, :class_name => "School", :foreign_key => "suspect_school_id"
 
+  geocoded_by :location
+  after_validation :geocode
+
+
   def suspect_name
     suspect_name = self.suspect_first_name + ' ' + self.suspect_last_name
+  end
+
+  def location
+    [street_number, street_name, city, ].compact.join(' ')
   end
 
 end
