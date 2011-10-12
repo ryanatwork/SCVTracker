2b6f8a51e79e24f4aefbbe734a8137532ca5ece7
Ryan Resella <ryan@codeforamerica.org>
2011-10-11 23:03:30 -0700

    Removed CrimeTipFile model

--- app/models/crime_tip_file.rb
+++
@@ -1,5 +0,0 @@
-class CrimeTipFile < ActiveRecord::Base
-  belongs_to :crime_tip
-  has_attached_file :crime_tip
-
-end
--- spec/models/crime_tip_file_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe CrimeTipFile do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
