4145a7549681d6026578f824dfa83e48b72dff81
Ryan Resella <ryan@codeforamerica.org>
2011-10-15 19:09:39 -0700

    don't post process non image files

--- app/models/tip_file.rb
+++ app/models/tip_file.rb
@@ -1,4 +1,11 @@
 class TipFile < ActiveRecord::Base
   belongs_to :crime_tip
   has_attached_file :tip
+
+  before_post_process :is_image?
+
+  def is_image?
+    ["image/jpeg", "image/pjpeg", "image/png", "image/x-png", "image/gif"].include?(@content_type)
+  end
+
 end
