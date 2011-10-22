fc6c685d764847dd2e620999e0f2c691af36d3b9
Ryan Resella <ryan@codeforamerica.org>
2011-10-22 13:06:45 -0700

    Check abilities on Student controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,7 @@
 class StudentsController < ApplicationController
   before_filter :authenticate_user!
+  check_authorization
+  load_and_authorize_resource
 
   # GET /students
   # GET /students.json
