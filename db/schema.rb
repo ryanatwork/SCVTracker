e53062b4ecf147562a8db8ca043f533244f627f6
Ryan Resella <ryan@codeforamerica.org>
2011-10-22 12:31:24 -0700

    Added RolesUser model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111022192655) do
+ActiveRecord::Schema.define(:version => 20111022193009) do
 
   create_table "assigns", :force => true do |t|
     t.string   "team"
@@ -125,6 +125,11 @@ ActiveRecord::Schema.define(:version => 20111022192655) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "roles_users", :id => false, :force => true do |t|
+    t.integer "role_id"
+    t.integer "user_id"
   end
 
   create_table "schools", :force => true do |t|
