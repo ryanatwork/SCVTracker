644da280f24b357ac64d99ba098d308aa9a0568e
Ryan Resella <ryan@codeforamerica.org>
2011-10-22 13:46:03 -0700

    Created moel for assignment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111022193435) do
+ActiveRecord::Schema.define(:version => 20111022204547) do
+
+  create_table "assignments", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "role_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "assigns", :force => true do |t|
     t.string   "team"
