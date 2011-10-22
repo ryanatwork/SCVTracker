34d751b5bd5801f58702927856c065387369fc06
Ryan Resella <ryan@codeforamerica.org>
2011-10-22 14:25:31 -0700

    Added roles for cancan in spec

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -3,6 +3,8 @@ module ControllerMacros
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
       @user = Factory.create(:user)
+      @role = Factory.create(:role)
+      @user.assignments.create(:role_id => 1)
       #user.confirm! # or set a confirmed_at inside the factory. Only necessary if you are using the confirmable module
       sign_in @user
     end
