6f3f26a749b60cfd2b1e4a6b386dd8cd22afdf7f
Ryan Resella <ryan@codeforamerica.org>
2011-10-22 15:17:16 -0700

    Added s3 configuration on models

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,9 +7,17 @@ class Student < ActiveRecord::Base
   belongs_to :updated_school, :class_name => "School", :foreign_key => "updated_school_id"
   belongs_to :deputy, :class_name => "User", :foreign_key => "school_deputy_id"
 
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" },
-                    :default_url => 'default_student.jpg'
-                    #:default_url => 'missing_:style.png'
+  if Rails.env  == 'development'
+    has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+      :default_url => 'default_student.jpg'
+  elsif Rails.env  == 'production'
+      has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+                    :default_url => 'default_student.jpg',
+                    :storage => :s3,
+                    :bucket => 'sheriff',
+                    :s3_credentials => {:access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET']}
+  end
+
 
   validates_presence_of :first_name
 
--- app/models/tip_file.rb
+++ app/models/tip_file.rb
@@ -1,8 +1,16 @@
 class TipFile < ActiveRecord::Base
   belongs_to :crime_tip
-  has_attached_file :tip, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 
-  before_post_process :is_image?
+  if Rails.env  == 'development'
+    has_attached_file :tip, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  elsif Rails.env  == 'production'
+      has_attached_file :tip, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+                    :storage => :s3,
+                    :bucket => 'sheriff',
+                    :s3_credentials => {:access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET']}
+  end
+
+  #before_post_process :is_image?
 
   def is_image?
     ["image/jpeg", "image/pjpeg", "image/png", "image/x-png", "image/gif"].include?(@content_type)
