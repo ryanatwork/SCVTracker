ce1ead1b3cfecf06b1e6acd7ddde40e156952a0e
Ryan Resella <ryan@codeforamerica.org>
2011-11-13 15:49:01 -0800

    Added admin to login_user

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -2,7 +2,7 @@ module ControllerMacros
   def login_user
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      @user = Factory.create(:user)
+      @user = Factory.create(:user, :admin => true)
       @role = Factory.create(:role)
       @user.assignments.create(:role_id => 1)
       #user.confirm! # or set a confirmed_at inside the factory. Only necessary if you are using the confirmable module
