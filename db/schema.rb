1a9ce8973d1eadafeb57819f5ce846eb68a97ffe
Ryan Resella <ryan@codeforamerica.org>
2011-11-13 19:08:05 -0800

    Added an approved status to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111113225819) do
+ActiveRecord::Schema.define(:version => 20111114025151) do
 
   create_table "assignments", :force => true do |t|
     t.integer  "user_id"
@@ -207,9 +207,6 @@ ActiveRecord::Schema.define(:version => 20111113225819) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
     t.integer  "failed_attempts",                       :default => 0
     t.string   "unlock_token"
     t.datetime "locked_at"
@@ -218,9 +215,9 @@ ActiveRecord::Schema.define(:version => 20111113225819) do
     t.string   "first_name"
     t.string   "last_name"
     t.boolean  "admin"
+    t.boolean  "approved"
   end
 
-  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
   add_index "users", ["unlock_token"], :name => "index_users_on_unlock_token", :unique => true
