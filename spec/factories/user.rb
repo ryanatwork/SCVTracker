fdd687f3e174254d0e82df8419143fd532c63e58
Ryan Resella <ryan@codeforamerica.org>
2011-11-13 19:12:47 -0800

    Added approved to user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,6 +3,6 @@ FactoryGirl.define do
     sequence(:email){|n| "email#{n}@test.com" }
     password "123456"
     password_confirmation "123456"
-    confirmed_at Date.new(2011,1,1)
+    approved true
   end
 end
